db476fdfcbdc0b9b788558bc6276fa0caf288bbb
Your Name <you@example.com>
2022-08-20 01:42:34 -0700

    included comments regarding the code and what each part does

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,10 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import renderer from 'react-test-renderer'
 
-
+    //set variable "tree" to renderer.create 
+   //renderer.create will render the element and create an output that contains toJSON method.
+  //the call toJSON() Turns the component representation into JSON.. MAKES EASIER TO SAVE AS A SNAPSHOT and compare to others
+ // last line compares new snapshot to the saved one (if it exists)
   
 
 test('renders a snapshot', () => {
